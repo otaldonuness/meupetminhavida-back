850b84b4ef4a9f599895b46285f52654ee51e827
William Rodrigues <williamrodrigues2442@gmail.com>
2023-06-17 14:13:27 -0300

    feat: creates decorator to get logged user info

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { JwtGuard } from 'src/shared/guards';
+import { GetUser } from 'src/shared/decorators';
+import { Users } from '@prisma/client';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @ApiBearerAuth()
   @Get('me')
-  getCurrentUser(@Req() req: Request) {
-    return req.user;
+  getCurrentUser(@GetUser() user: Users) {
+    return user;
   }
 }
