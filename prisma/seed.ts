9d7a6f8df78e37e79cf5b681ccb9e934eb56b751
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2023-06-28 23:33:05 -0300

    fix(fix location db model and restore scripts to add locations fixtures): fix location model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,18 @@
-// import { PrismaClient } from '@prisma/client';
-// import locations = require('./seeds/location');
+import { PrismaClient } from "@prisma/client";
+import locations = require("./seeds/location");
 
-// const prisma = new PrismaClient();
-// async function main() {
-//   await prisma.locations.createMany({
-//     data: locations.data,
-//   });
-// }
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.locations.createMany({
+    data: locations.data,
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
