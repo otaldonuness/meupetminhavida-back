89de14a2279636fb84496ff66de28dbf84058c47
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2023-06-30 19:31:16 -0300

    feat: error handling to controller has been added

--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, NotFoundException, Param, Res } from "@nestjs/common";
 import { LocationsService } from "./locations.service";
 import {
   GetLocationInputDto,
   GetLocationOutputDto,
 } from "./dto/get.location.dto";
 import { PublicRoute } from "src/shared/decorators";
+import { response } from "express";
 
 @PublicRoute()
 @Controller("locations")
@@ -15,7 +16,14 @@ export class LocationsController {
   async findByState(@Param("id") id: string): Promise<GetLocationOutputDto> {
     const input = new GetLocationInputDto();
     input.id = id;
-    return await this.locationsService.findByID(input);
+    try {
+      return await this.locationsService.findByID(input);      
+    } catch(error) {
+      if(error instanceof NotFoundException) {
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND, { cause: new Error(error.message)});        
+      }
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR, { cause: new Error(error.message)});
+    }
   }
 
   // @Get(":id")
--- src/modules/locations/locations.service.ts
+++ src/modules/locations/locations.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import {
   GetLocationInputDto,
   GetLocationOutputDto,
 } from "./dto/get.location.dto";
 import { PrismaService } from "src/config/prisma/prisma.service";
 import { GetLocationByStateInputDto } from "./dto/get.locations.by.state.dto";
+import { NotFoundError } from "rxjs";
 
 @Injectable()
 export class LocationsService {
   constructor(private prisma: PrismaService) {}
 
   async findByID(input: GetLocationInputDto): Promise<GetLocationOutputDto> {
-    const location = await this.prisma.locations.findFirstOrThrow({
-      where: { id: input.id },
-    });
-
-    const output = new GetLocationOutputDto();
-    output.id = location.id;
-    output.city = location.city;
-    output.state = location.state;
-
-    return output;
+    try {
+      const location = await this.prisma.locations.findFirstOrThrow({
+        where: { id: input.id },
+      });
+      const output = new GetLocationOutputDto();
+      output.id = location.id;
+      output.city = location.city;
+      output.state = location.state;
+      return output;
+    } catch (error) {
+      if (error.constructor.name === "NotFoundError") {
+        console.log("locations:get:: city not found");
+        throw new NotFoundException("city not found");
+      }
+      console.log("locations:get:: unexpected error:" + error);
+      throw new Error("unexpected error");
+    }
   }
 
   findByState(input: GetLocationByStateInputDto) {
