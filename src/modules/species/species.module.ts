d038137c8cb6d0fd7a10dc1b625a48ff27c8d11f
mah <zanzinzzo@gmail.com>
2023-07-17 22:06:46 -0300

    feat: add species module

---
+++ src/modules/species/species.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { SpeciesController } from "./species.controller";
+import { SpeciesService } from "./species.service";
+
+@Module({
+  controllers: [SpeciesController],
+  providers: [SpeciesService]
+})
+export class SpeciesModule { }
