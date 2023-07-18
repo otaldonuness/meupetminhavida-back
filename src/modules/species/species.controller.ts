95c687ddfe86b498d957f018d341be2440189d88
mah <zanzinzzo@gmail.com>
2023-07-17 22:00:27 -0300

    feat: add species controller

---
+++ src/modules/species/species.controller.ts
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param, Post } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { SpeciesService } from "./species.service";
+import { CreateSpeciesDto } from "./dto";
+
+@ApiBearerAuth()
+@ApiTags("species")
+@Controller("species")
+export class SpeciesController {
+  constructor(private readonly speciesService: SpeciesService) { }
+
+  @Post()
+  create(@Param("name") name: CreateSpeciesDto) {
+    return this.speciesService.create(name)
+  }
+  
+  @Post("delete")
+  delete(@Param("id") id: string){
+    return this.speciesService.delete(id)
+  }
+
+  @Post("update")
+  update(@Param("species") species: CreateSpeciesDto, @Param("id") id: string){
+    return this.speciesService.update(species, id)
+  }
+
+  @Get()
+  getAll() {
+    return this.speciesService.getAll();
+  }
+
+  @Get(":id")
+  getByID(@Param("id") id: string) {
+    this.speciesService.getById(id)
+  }
+
+}
