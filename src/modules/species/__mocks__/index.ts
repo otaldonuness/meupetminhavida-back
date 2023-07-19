b9acf2b8225465df9f17fc3e01a84e733590eee5
mah <zanzinzzo@gmail.com>
2023-07-19 15:50:19 -0300

    test: add species' mocks and stub

---
+++ src/modules/species/__mocks__/index.ts
@@ -0,0 +1,1 @@
+export * from "./species.service.ts";
---
+++ src/modules/species/stubs/species.stub.ts
@@ -0,0 +1,7 @@
+import { CreateSpeciesDto } from "../dto";
+
+export const speciesStub = (): CreateSpeciesDto => {
+  return {
+    name: "corvus corax",
+  };
+};
